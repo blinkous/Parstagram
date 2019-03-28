6ad4e9c56cf5dbb0336b7964c2779aea62f1bbaa
blinkous <saradhani1@gmail.com>
2019-03-28 15:40:01 -0400

    Added comments

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by stargaze on 3/28/19.
+//  Copyright © 2019 blinkous. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -30,7 +30,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className:"Posts")
-        query.includeKey("author")
+        // Allows us to load the objects: get the author, comments and for each comment the author related to it
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground { (posts, error) in
@@ -43,27 +44,53 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Since we are working with sections, we no longer use indexPath.row to get the post, we use section
+        let post = posts[section]
+        
+        // Since there is a chance that there is nothing in the comments section, if the optional (in the parenthesis) is nil set it to []
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        return comments.count + 1
+    }
+    
+    // Give each post a section that can have any number of rows
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-        
         // Grab the data from the dictionary
-        let post = posts[indexPath.row]
-        let user = post["author"] as! PFUser
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        // Set the labels
-        cell.usernameLabel.text = user.username
-        cell.captionLabel.text = post["caption"] as? String
-        
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)
-        
-        cell.photoView.af_setImage(withURL: url!)
-        
-        return cell
+        // The post cell is always the 0th row, so here we set the post cell
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            let user = post["author"] as! PFUser
+            
+            // Set the labels
+            cell.usernameLabel.text = user.username
+            cell.captionLabel.text = post["caption"] as? String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)
+            
+            cell.photoView.af_setImage(withURL: url!)
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as! String
+            
+            let user = comment["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            
+            return cell
+        }
     }
     @IBAction func onLogoutButton(_ sender: Any) {
         // Log out the parse user
@@ -80,6 +107,29 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         delegate.window?.rootViewController = loginViewController
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Adding fake comments
+        //Choose a post to add comments to
+        let post = posts[indexPath.row]
+        
+        // Create the comments table if it's not there yet
+        let comment = PFObject(className: "Comments")
+        // Add text, the post, and author that the comment belongs to
+        comment["text"] = "Random comment booyah!"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        // Adding an array of comments to each comment
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
